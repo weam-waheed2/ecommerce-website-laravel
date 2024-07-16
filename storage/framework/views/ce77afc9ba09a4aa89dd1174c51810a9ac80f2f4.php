6b8ac70ec1747553b718a6d30986c9b88e30f28a
Weam Whaeed <80516034+weam-waheed2@users.noreply.github.com>
2024-07-16 10:57:37 +0300

    Delete storage/framework directory

--- storage/framework/views/ce77afc9ba09a4aa89dd1174c51810a9ac80f2f4.php
+++
@@ -1,7 +0,0 @@
-<?php $__currentLoopData = $terms; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $term): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-    <option <?php echo e($term->ID == $selected ? 'selected' : ''); ?> value="<?php echo e($term->ID); ?>"><?php echo e(str_repeat('—', $depth)); ?> <?php echo e($term->name); ?></option>
-    <?php if(count($term->childs())): ?>
-        <?php echo $__env->make('admin.terms.items.select',['terms' => $term->childs(), 'depth' => $depth + 1, 'selected' => $selected], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    <?php endif; ?>
-<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-<?php /**PATH C:\xampp\htdocs\siam_ocean_app\resources\views/admin/terms/items/select.blade.php ENDPATH**/ ?>
